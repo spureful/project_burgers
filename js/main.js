d771184ff7e9ed0244641f3221cda8e134a1db24
spureful <rw1zz@yzandex.ru>
2018-05-08 21:32:39 +0300

    make correction homework_week_2

---
+++ js/main.js
@@ -0,0 +1,46 @@
+function openMenu() {
+	const openBtn = document.querySelector('.menu__hamburger');
+	
+	const menu = $('.hamburgermenu');
+	const closeBtn = document.querySelector('.hamburgermenu__close');
+	const wrapper = document.querySelector('.wrapper');
+	
+	openBtn.addEventListener('click', function(){
+		menu.fadeIn();
+		menu.addClass('hamburgermenu_visible');
+		
+		
+		wrapper.style.overflow = 'hidden';
+	}					
+							);
+
+	closeBtn.addEventListener('click', function(){
+		menu.fadeOut().removeClass('hamburgermenu_visible');
+		wrapper.style.oveflow ='visible';
+	});
+	
+	}
+
+
+
+openMenu();
+
+function openPosition(){
+	const openPos = document.querySelector('.screenmenu__position');
+	const posContent = $('.screenmenu__content');
+	
+	const closePos = document.querySelector('.screenmenu__position');
+	
+	
+	openPos.addEventListener('click', function(){
+//		posContent.fadeIn();
+		posContent.addClass('screenmenu__content-active');
+	});
+	
+//	closePos.addEventListener('click', function(){
+//			posContent.removeClass('screenmenu__content-active');
+//	})
+};
+
+
+openPosition()
