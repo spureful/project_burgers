07a7602ba41e4f372f81ebf1a81af43e4982c1a2
spureful <rw1zz@yzandex.ru>
2018-05-11 17:03:42 +0300

    homework_week_2 edited

--- js/main.js
+++ js/main.js
@@ -57,18 +57,51 @@ openComposition();
 
 
 function openPosition(){
-	const openPos = document.querySelectorAll('.screenmenu_accordeon>.screenmenu__item');
-	const posContent = document.querySelector('.screenmenu__content');
+	const openPos = document.querySelector('.screenmenu__item');
+	const posContent = $('.screenmenu__content');
 	
 	
 	
 	
 	openPos.addEventListener('click', function(){
 
-		posContent.classList.toggle('screenmenu__content-active');
+		posContent.fadeIn();
+		posContent.toggleClass('screenmenu__content-active');
 	});
 
 };
 
 
 openPosition()
+
+
+function commentsPopup() {
+	var name = document.querySelector('.screencomments__title');
+	var namePopup = document.querySelector('.screencomments__popup-name');	
+	var comment = document.querySelector('.screencomments__text');
+	var commentPopup = document.querySelector('.screencomments__popup-content');
+	const openPop = document.querySelector('.screencomments__button');
+	const popup = document.querySelector('.screencomments__popup');
+	const closePop = document.querySelector('.screencomments__popup-close');
+	let op = 0;
+	
+	openPop.addEventListener('click', function(){
+		popup.classList.add('screencomments__popup_active');
+	});
+	
+	namePopup.contentText= name.contentText;
+	closePop.addEventListener('click', function(){
+		popup.classList.remove('screencomments__popup_active');
+	});
+	
+		setTimeout (function foo(){
+		if (op < 1){
+			popup.style.opacity = op;
+			op += 0.1;
+			setTimeout(foo, 100);
+		}
+	}, 100);
+	
+};
+
+commentsPopup()
