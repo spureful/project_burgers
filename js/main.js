7eac31dfcab2fb0b171c3752584d96e5b6db2ff3
spureful <rw1zz@yzandex.ru>
2018-05-13 18:09:49 +0300

    homework_week_3

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
 function openMenu() {
 	const openBtn = document.querySelector('.menu__hamburger');
-	
+
 	const menu = $('.hamburgermenu');
 	const closeBtn = document.querySelector('.hamburgermenu__close');
 	const wrapper = document.querySelector('.wrapper');
-	
-	openBtn.addEventListener('click', function(){
+
+	openBtn.addEventListener('click', function () {
 		menu.fadeIn();
 		menu.addClass('hamburgermenu_visible');
-		
-		
-		wrapper.style.overflow = 'hidden';
-	}					
-							);
 
-	closeBtn.addEventListener('click', function(){
-		menu.fadeOut().removeClass('hamburgermenu_visible');
-		wrapper.style.oveflow = 'none';
+
+		wrapper.style.overflow = 'hidden';
 	});
-	
-	}
+
+	closeBtn.addEventListener('click', function () {
+		menu.fadeOut().removeClass('hamburgermenu_visible');
+		wrapper.style.overflow = 'none';
+	});
+
+}
 
 
 
@@ -32,88 +31,85 @@ function openComposition() {
 	var appearMenu = document.querySelector(".screenslider__composition");
 	var closeBtn = document.querySelector(".screenslider__productcontent");
 	let op = 0;
-	
-	openBtn.addEventListener("click", function(){
+
+	openBtn.addEventListener("click", function () {
 		appearMenu.classList.toggle("screenslider__composition_active")
-		
-	openBtn.style.backgroundColor = "#e35028";
+
+		openBtn.style.backgroundColor = "#e35028";
 
 	});
-	
-	setTimeout (function foo(){
-		if (op < 1){
+
+	setTimeout(function foo() {
+		if (op < 1) {
 			appearMenu.style.opacity = op;
 			op += 0.1;
 			setTimeout(foo, 100);
 		}
 	}, 100);
-	
 
-	
-	}
-	
+
+
+}
+
 
 openComposition();
 
 
-function openPosition(){
-	const openPos = document.querySelector('.screenmenu_accordeon');
-	const posContent = document.querySelector('.screenmenu__content');
-	
-	
-	
-	openPos.addEventListener('click', function(event){
-		if( event.target.className = "screenmenu__position-btn"){
-			posContent.classList.toggle('screenmenu__content-active');
-		};
+function openPosition() {
+	const openPos = document.getElementsByClassName('screenmenu__position-btn');
+	const posContent = document.querySelector('.screenmenu__position');
+
+
+	for (var i = 0; i < openPos.length; i++) {
+		openPos[i].addEventListener('click', function () {				
+			
+			posContent.classList.toggle('screenmenu__position_active')
+			});
 
 		
-//		posContent.fadeIn();
-//		posContent.toggleClass('screenmenu__content-active');
-	});
 	};
-//};
+}
 
 
-openPosition()
+openPosition();
 
 
 function commentsPopup() {
 	var name = document.querySelector('.screencomments__title');
-	var namePopup = document.querySelector('.screencomments__popup-name');	
+	var namePopup = document.querySelector('.screencomments__popup-name');
 	var comment = document.querySelector('.screencomments__text');
 	var commentPopup = document.querySelector('.screencomments__popup-content');
 	const openPop = document.querySelector('.screencomments__button');
 	const popup = document.querySelector('.screencomments__popup');
 	const closePop = document.querySelector('.screencomments__popup-close');
 	let op = 0;
-	
-	
-	
-//	for(var i=0; i< openPop.length; i++){
-	openPop.addEventListener('click', function(){
-		console.log(openPop.length);
+
+
+
+	openPop.addEventListener('click', function () {
+
 		popup.classList.add('screencomments__popup_active');
-		
+
 		namePopup.textContent = name.innerHTML;
 		commentPopup.textContent = comment.innerHTML;
 	});
-//	}
 	
-	
-	
-	closePop.addEventListener('click', function(){
+
+
+
+	closePop.addEventListener('click', function () {
 		popup.classList.remove('screencomments__popup_active');
 	});
-	
-		setTimeout (function foo(){
-		if (op < 1){
+
+	setTimeout(function foo() {
+		if (op < 1) {
 			popup.style.opacity = op;
 			op += 0.1;
 			setTimeout(foo, 100);
 		}
 	}, 100);
-	
+
 };
 
 commentsPopup()
+
